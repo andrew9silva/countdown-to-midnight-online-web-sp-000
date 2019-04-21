767cf2ccc4504537c5a75a62fc2c952fd85f19a1
Andrew Silva <andrewsilva9@gmail.com>
2019-04-21 16:21:17 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,5 +10,12 @@ def countdown(counter)
 end
 
 def countdown_with_sleep(seconds)
+  counter = 10
+  while counter >0
+    puts "#{counter} SECOND(S)!"
+  counter -= 1
+  sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
   
 end
